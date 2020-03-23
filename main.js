54a8522e9043f8ffaa10c3127a96cffeed563dec
Iqra Fatima <46266421+iqrafatimame@users.noreply.github.com>
2020-03-23 05:03:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn =  document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+setInterval(() => {
+    let day = new Date();
+    let h = day.getHours() * 30;
+    let m = day.getMinutes() * deg;
+    let s = day.getSeconds() * deg;
+    hr.style.transform = `rotateZ(${h + (m/12)}deg)`;
+    mn.style.transform = `rotateZ(${m}deg)`;
+    sc.style.transform = `rotateZ(${s}deg)`;
+});
+
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let time = h + ":" + m + ":" + s;
+    document.getElementById("digitalClock").innerText = time;
+    document.getElementById("digitalClock").textContent = time;
+}
+showTime();
+   
